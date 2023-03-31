cd89b51b5e5371eb63189238a8e8acaa3c5058fa
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-03-31 01:02:36 -0300

    Refactor struct example

--- more-types/struct/struct3.go
+++ more-types/struct/struct3.go
@@ -6,18 +6,27 @@ type Person struct {
 	name string
 }
 
-/* Here the change stays inside the func */
+// Here the change stays inside the func
+// and ends up mutating the copy
 func rename(person Person) {
 	person.name = "test"
 }
 
-/* Here we mutate the atrr inside the function */
+// Here we mutate the atrr inside the function
+// throug the struct pointer
 func renameV2(person *Person) {
 	// (*person).name = "" // longer way to access struct pointer fields
-	person.name = "test"
+	person.name = "test2"
 }
 
-/* One good reason to use pointers instead of pass-by-values */
+// in a simple way, similar to how append works
+// we can mutate the copy and then return it
+func renameV3(person Person) Person {
+	person.name = "test3"
+	return person
+}
+
+// One example of pointers x pass-by-values
 func main() {
 	p := Person{"Lala"}
 	fmt.Println(p.name)
@@ -32,4 +41,9 @@ func main() {
 	fmt.Println(p2.name)
 	renameV2(&p2)
 	fmt.Println(p2.name)
+
+	p3 := Person{"james"}
+	fmt.Println(p3.name)
+	p3 = renameV3(p3)
+	fmt.Println(p3.name)
 }
