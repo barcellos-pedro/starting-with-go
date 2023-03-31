93e6bd4660cd8fbabb18a9d0713e479fc90afcd4
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-03-31 00:45:50 -0300

    Add struct example

---
+++ more-types/struct/struct3.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name string
+}
+
+/* Here the change stays inside the func */
+func rename(person Person) {
+	person.name = "test"
+}
+
+/* Here we mutate the atrr inside the function */
+func renameV2(person *Person) {
+	// (*person).name = "" // longer way to access struct pointer fields
+	person.name = "test"
+}
+
+/* One good reason to use pointers instead of pass-by-values */
+func main() {
+	p := Person{"Lala"}
+	fmt.Println(p.name)
+	rename(p)
+	fmt.Println(p.name)
+
+	var p2 *Person = &Person{"peter"}
+	fmt.Println(p2.name)
+	renameV2(p2)
+	fmt.Println(p2.name)
+}
