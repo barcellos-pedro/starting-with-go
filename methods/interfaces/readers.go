10580cdf0769db8560cacb22745067cc93ae7fd4
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-05-03 00:46:05 -0300

    Add Readers example

---
+++ methods/interfaces/readers.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+func main() {
+	reader := strings.NewReader("Hello world")
+	// using the exact number of bytes (11)
+	// buffer := make([]byte, reader.Len())
+	buffer := make([]byte, 4)
+
+	// read the string content 4 bytes at a time
+	for {
+		bytesRead, err := reader.Read(buffer)
+
+		fmt.Printf("Bytes read: %v\n", bytesRead)
+		fmt.Printf("Content: %q\n", buffer[:bytesRead])
+
+		// Error that indicates end of file
+		if err == io.EOF {
+			break
+		}
+	}
+}
