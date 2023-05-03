5712048fc55cddc4011feb45571364d2d0f2a468
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-05-02 23:44:47 -0300

    Add Errors example

---
+++ methods/interfaces/errors.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// not necessary, just to be explicit
+type Error interface {
+	Error() string
+}
+
+type MyError struct {
+	Cause string
+}
+
+// implementing the method
+func (err *MyError) Error() string {
+	return fmt.Sprintf("MyError error: %v", err.Cause)
+}
+
+func sum(a, b int) (int, *MyError) {
+	if a == 0 && b == 0 {
+		return 0, &MyError{"I won't sum zero values."}
+	}
+
+	return a + b, nil
+}
+
+func main() {
+	if result, err := sum(0, 0); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Result:", result)
+	}
+}
