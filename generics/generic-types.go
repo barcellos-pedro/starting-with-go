bd44055326f0d30c0e3a75a6ac2bd4b49ecfa1f7
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-05-11 11:34:44 -0300

    Refactor Generics types example

--- generics/generic-types.go
+++ generics/generic-types.go
@@ -8,48 +8,79 @@ type List[T any] struct {
 	next  *List[T] // pointer to avoid error: List refers to itself
 }
 
+// interface declaration to specify methods for the struct
+type SingleLinkedList[T any] interface {
+	newList() *List[T]
+	peek() T
+	values() []T
+	push(val T)
+}
+
 func (*List[T]) newList() *List[T] {
 	return &List[T]{}
 }
 
-func (list *List[T]) append(val T) {
-	for v := list.next; v.next != nil; v = v.next {
-		v.value = val
-	}
+func (list *List[T]) peek() T {
+	return list.value
 }
 
-func (list *List[T]) getAll() []T {
-	result := make([]T, 0)
-	result = append(result, list.value)
-
-	for {
-		if node := list.next; node.next == nil {
-			break
-		} else {
-			result = append(result, node.value)
-		}
+func (list *List[T]) values() []T {
+	if list.next == nil {
+		return []T{list.value}
 	}
 
-	return result
+	values := make([]T, 0)
+	values = append(values, list.value)
+
+	node := list.next
+
+	// while node is not nil
+	for node != nil {
+		values = append(values, node.value)
+		node = node.next
+	}
+
+	return values
+}
+
+func (list *List[T]) push(val T) {
+	newElement := list.newList()
+	newElement.value = val
+
+	if list.next == nil {
+		list.next = newElement
+		return
+	}
+
+	node := list.next
+
+	for {
+		if node.next == nil {
+			node.next = newElement
+			break
+		}
+		node = node.next
+	}
 }
 
 func main() {
 	var comments List[string]
-	comments.value = "1º"
-	fmt.Println(comments)
+	comments.value = "nice"
+	comments.push("cool")
+	comments.push("dope")
 
-	comments.append("2º")
-	fmt.Println(comments)
+	// Testing methods
+	fmt.Println("Peek:", comments.peek())
+	fmt.Println("All values:", comments.values())
+	fmt.Println("Range")
+	for _, comment := range comments.values() {
+		fmt.Println(comment)
+	}
 
-	// second := comments.newList()
-	// second.value = "2º"
-	// comments.next = second
-	// fmt.Println(comments)
-
-	// third := comments.newList()
-	// third.value = "3º"
-	// comments.next.next = third
-	// fmt.Println(comments)
-
-	// fmt.Println("All values", comments.getAll())
+	// Using an interface
+	// pointer to comments because of methods that have pointer receivers
+	var i SingleLinkedList[string] = &comments
+	fmt.Println("Using Interface")
+	fmt.Println(i.peek())
+	fmt.Println(i.values())
 }
