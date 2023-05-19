34682bbf7a400bd90045900e7c68d676206d07c3
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-05-19 01:17:27 -0300

    Add comments

--- concurrency/channels/channels-explained-2.go
+++ concurrency/channels/channels-explained-2.go
@@ -6,6 +6,7 @@ import "fmt"
 //
 // When a goroutine receives data from a channel, the operation is blocked until another goroutine sends the data to the channel.
 func receiveData(ch chan string) {
+	fmt.Println("Receiving data from the channel...")
 	// receive data from the channel
 	// blocks here until the main or another goroutinte sends data to the channel
 	fmt.Println(<-ch)
@@ -18,7 +19,7 @@ func main() {
 	// function call with goroutine
 	go receiveData(ch)
 
-	fmt.Println("No data. Receive Operation Blocked")
+	fmt.Println("No data sent yet. Receive Operation Blocked")
 
 	// send data to the channel
 	// the goroutine of receiveData() can continue its execution
--- concurrency/channels/channels-explained.go
+++ concurrency/channels/channels-explained.go
@@ -6,7 +6,10 @@ import "fmt"
 //
 // When a goroutine sends data into a channel, the operation is blocked until the data is received by another goroutine.
 func sendData(ch chan string) {
+	fmt.Println("Sending data to the channel...")
 	ch <- "Received. Send Operation Successful"
+	// Data is sent and the operation blocks from here
+	// until the main or another goroutinte receives the data from the channel
 	fmt.Println("No receiver! Send Operation Blocked")
 }
 
