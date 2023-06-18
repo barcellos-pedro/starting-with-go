c98332728341cd8b11359dac6d508496a51b6e6d
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-06-18 13:58:13 -0300

    Refactor template example

--- http/templates/app.go
+++ http/templates/app.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"os"
 )
@@ -21,34 +20,44 @@ type PageData struct {
 }
 
 var (
-	PORT = 8080
-	html string
+	data  PageData
+	templ *template.Template
 )
 
 func handleError(err error) {
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
 
+func getHome(response http.ResponseWriter, request *http.Request) {
+	templ.Execute(response, data)
+}
+
+func getView(fileName string) (string, error) {
+	file, err := os.ReadFile(fileName)
+	return string(file), err
+}
+
+func getTemplate(templName, html string) *template.Template {
+	return template.Must(template.New(templName).Parse(html))
+}
+
 func main() {
-	if file, err := os.ReadFile("index.html"); err != nil {
-		handleError(err)
-	} else {
-		html = string(file)
-	}
-
+	// Set page data
 	person := Person{Name: "Pedro", Age: 26, Alive: true, Hobbies: []string{"Movies", "Restaraunts"}}
-	data := PageData{Title: "HTML Templates", Data: person}
-	templ := template.Must(template.New("home").Parse(html))
+	data = PageData{Title: "HTML Templates", Data: person}
 
-	// Show template rendered result on console
-	templ.Execute(os.Stdout, data)
+	// Get template file content
+	html, err := getView("index.html")
+	handleError(err)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		templ.Execute(w, data)
-	})
+	templ = getTemplate("home", html)
+
+	// Write rendered template on console
+	// templ.Execute(os.Stdout, data)
 
 	fmt.Println("\n⚡ Server on http://localhost:8080")
+	http.HandleFunc("/", getHome)
 	http.ListenAndServe(":8080", nil)
 }
